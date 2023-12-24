fc58291fa4deb080856f1068eaac0341e6d3a840
paulwenny <pollywenny@gmail.com>
2023-12-25 05:32:26 +0800

    03:Fungsi Output di JS 1. Membuat folder 03 yang berisi file index.html, main.js, note03.txt 2. file index.html berisi struktur html yang memanggil fungsi yang di buat di file main.js (meskipun belum pada materi inplementasi function) 3. file 03 berisi ringkasan materi ke 03:fungsi output di JS

---
+++ 03-fungsi_output_di_js/main.js
@@ -0,0 +1,21 @@
+function output_Console(){
+    console.log("ini pesan console");
+}
+
+function output_Alert(){
+    alert("ini pesan Alert");
+}
+
+function output_DWrite(){
+    document.write('Ini Pesan Document Write');
+}
+
+function in_Html(){
+    var elemenId = document.getElementById('Implemetasi');
+
+    // elemenId.innerHTML = "<p>Berhasil di Implemetasi</p>";
+
+    // alert('elemenId.innerHTML = "<p>Berhasil di Implemetasi</p>"');
+
+    console.log('elemenId.innerHTML = "<p>Berhasil di Implemetasi</p>"');
+}
