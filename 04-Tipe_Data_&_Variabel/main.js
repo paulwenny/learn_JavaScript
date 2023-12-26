6ae0059e7c11de26b7aa09bbe62ced33e4cc54b7
paulwenny <pollywenny@gmail.com>
2023-12-26 17:18:29 +0800

    04 Tipe Data dan Variabel 1. membuat folder 04 yang berisi index.html, main.js, note04.txt 2. file index.html dan main.js merupakan file yang berisi implemantasi cara penulusan variabel dan tipe data dalam js 3. file note.js merupakan note atau ringkasan dari materi ke 04 yang ada di website petanikode.com tentang tipe data dan variabel di js

---
+++ 04-Tipe_Data_&_Variabel/main.js
@@ -0,0 +1,15 @@
+var iniVariabel = "Menampilkan isi Variabel"
+
+no_name = "ini variabel tanpa kata kunci"
+
+// menghapus variabel
+delete no_name;
+
+// cara mengecek tipe data
+typeof iniVariabel;
+
+document.write(iniVariabel + '<br>')
+
+document.write(no_name)
+
+
